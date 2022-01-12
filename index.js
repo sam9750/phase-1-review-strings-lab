74b9ddcad05ba4b6bf83534ff21d527ce3e7f2f3
sam9750 <sam.wadhwa95@gmail.com>
2022-01-12 02:49:17 -0500

    commit tree b0ffbe16108e80952a4ce1ad4d95c91563a6fb3c

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser = `Sam`;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; 
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting1 = `Welcome, ${currentUser}`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`; //gets first letter of current user's name, starts at index 0 and goes to index 1
+
+
